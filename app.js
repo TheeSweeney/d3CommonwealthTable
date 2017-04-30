ba4c20a8ec1b3ca5d065f9191b406bc9f98fc836
Brian Sweeney <bsweeney930@gmail.com>
2017-04-30 00:30:38 -0400

    chart styling clicking open section will close it

--- app.js
+++ app.js
@@ -12,10 +12,18 @@ $( document ).ready(function(){
         $(element).find("#headerRow").addClass('odd')
 
         $(element).find("tr.odd").click(function() {
+            var alreadyActive = false;
+            if($(this).next("tr").find('td').attr('id') === 'activeRow') alreadyActive = true;
+
             $(element).find("td").attr('id', '');
             $(this).next("tr").find('td').attr('id','activeRow')
             $(element).find("tr:not(.odd)").hide();
             $(this).next("tr").toggle();
+
+            if(alreadyActive){
+              $(this).next('tr').toggle();
+              $(this).next("tr").find('td').attr('id','')
+            } 
 
             createChart();
         });
