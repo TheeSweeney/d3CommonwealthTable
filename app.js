b3b1a4310de75b3ffaee9f5a69a9f3f5003c4516
Brian Sweeney <bsweeney930@gmail.com>
2017-04-29 21:29:43 -0400

    chart rendering in expanded row

--- app.js
+++ app.js
@@ -20,22 +20,22 @@ $('#exhibitTable').jExpand()
 
 
 
-// var w = 800;
-// var h = 450;
-// var margin = {
-//   top: 58,
-//   bottom: 100,
-//   left: 80,
-//   right: 40
-// };
-// var width = w - margin.left - margin.right;
-// var height = h - margin.top - margin.bottom;
+var w = 400;
+var h = 225;
+var margin = {
+  top: 58,
+  bottom: 100,
+  left: 80,
+  right: 40
+};
+var width = w - margin.left - margin.right;
+var height = h - margin.top - margin.bottom;
 
-// var svg = d3.select("body").append("svg")
-//       .attr("id", "chart")
-//       .attr("width", w)
-//       .attr("height", h);
-// var chart = svg.append("g")
-//       .classed("display", true)
-//       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var svg = d3.select(".oddRow").append("svg")
+      .attr("id", "chart")
+      .attr("width", w)
+      .attr("height", h);
+var chart = svg.append("g")
+      .classed("display", true)
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 })
