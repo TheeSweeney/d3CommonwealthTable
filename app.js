b31e8e4394ea243de8697fcae42f04acb91e18ef
Brian Sweeney <bsweeney930@gmail.com>
2017-04-29 21:00:43 -0400

    basic table created with actual information

--- app.js
+++ app.js
@@ -16,3 +16,17 @@
 // var chart = svg.append("g")
 //       .classed("display", true)
 //       .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+(function($){
+    $.fn.jExpand = function(){
+        var element = this;
+
+        $(element).find("tr:odd").addClass("odd");
+        $(element).find("tr:not(.odd)").hide();
+        $(element).find("tr:first-child").show();
+
+        $(element).find("tr.odd").click(function() {
+            $(this).next("tr").toggle();
+        });
+        
+    }    
+})(jQuery); 
