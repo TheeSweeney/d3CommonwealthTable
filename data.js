de44ec158fde8838ff968f07ee90c38d4ae446fc
Brian Sweeney <bsweeney930@gmail.com>
2017-05-01 18:27:32 -0400

    d3 table with real data rendering, albeit off axis

---
+++ data.js
@@ -0,0 +1,73 @@
+var dataset2 = [
+  {"header": "OVERALL RANKING" ,"AUS": 1, "CAN": 9, "FRA": 10, "GER": 8, "NETH": 3, "NZ": 4, "NOR": 5, "SWE": 6, "SWIZ": 6, "UK": 2, "US": 1  }, 
+  {"header": "Quality", "AUS": 2, "CAN": 6, "FRA": 9, "GER": 8, "NETH": 4, "NZ": 3, "NOR": 10, "SWE": 11, "SWIZ": 7, "UK": 1, "US":  5 }, 
+  {"header": "Access", "AUS": 4, "CAN": 10, "FRA": 9, "GER": 2, "NETH": 1, "NZ": 7, "NOR": 5, "SWE": 6, "SWIZ": 8, "UK": 3, "US": 1  }, 
+  {"header": "Administrative Burden", "AUS": 1, "CAN": 6, "FRA": 11, "GER": 6, "NETH": 9, "NZ": 2, "NOR": 4, "SWE": 5, "SWIZ": 8, "UK": 3, "US": 1  }, 
+  {"header": "Equity", "AUS": 7, "CAN": 9, "FRA": 10, "GER": 6, "NETH": 2, "NZ": 8, "NOR": 5, "SWE": 3, "SWIZ": 4, "UK": 1, "US": 1  }, 
+  {"header": "Health Care Outcomes", "AUS": 1, "CAN": 9, "FRA": 5, "GER": 8, "NETH": 6, "NZ": 7, "NOR": 3, "SWE": 2, "SWIZ": 4, "UK": 10, "US": 1  }, 
+  {"header": "Health Spending per Capita*" ,"AUS": "$4,207" ,"CAN": "$4,724" ,"FRA": "$4,620" ,"GER": "$5,119" ,"NETH": "$5,277" ,"NZ": "$4,038" ,"NOR": "$6,432" ,"SWE": "$5,306" ,"SWIZ": "$6,787" ,"UK": "$4,094" ,"US": "$9,364"}
+]
+
+var dataset3 = [
+    {country: '', overall: 'Overall', access: 'Access', efficiency: 'Administrative Efficiency', equity: 'Equity', hco: 'Health Care Outcomes', spending: 'Health Spending per Capita'},
+    {country: 'AUS', overall: 1, quality: 2, access: 4, efficiency: 1, equity: 7, outcomes: 1, spending: "$4,207"},
+    {country: 'CAN', overall: 9, quality: 6, access: 10, efficiency: 6,  equity: 9, outcomes: 9, spending: "$4,724"},
+    {country: 'FRA', overall: 10, quality: 9, access: 9, efficiency: 11, equity: 10, outcomes: 5, spending: "$4,620"},
+    {country: 'GER', overall: 8, quality: 8, access: 2, efficiency: 6, equity: 6, outcomes: 8, spending: "$5,119"},
+    {country: 'NETH', overall: 3, quality: 4, access: 1, efficiency: 9, equity: 2, outcomes: 6, spending: "$5,277"},
+    {country: 'NZ', overall: 4, quality: 3, access: 7, efficiency: 2, equity: 8, outcomes: 7, spending: "$4,038"},
+    {country: 'NOR', overall: 5, quality: 10,access: 5, efficiency: 4, equity: 5, outcomes: 3, spending: "$6,432"},
+    {country: 'SWE', overall: 6, quality: 11,access: 6, efficiency: 5, equity: 3, outcomes: 2, spending: "$5,306"},
+    {country: 'SWIZ', overall: 6, quality: 7, access: 8, efficiency: 8, equity: 4, outcomes: 4, spending: "$6,787"},
+    {country: 'UK', overall: 2, quality: 1, access: 3, efficiency: 3, equity: 1, outcomes: 10, spending: "$4,094"},
+    {country: 'US', overall: 11, quality: 5, access: 11, efficiency: 10,  equit: 11, outcomes: 11, spending: "$9,364"},
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- table.js
+++ table.js
@@ -8,6 +8,25 @@ for (i = 0; i < 5; i++) {
     }
     dataset.push(tmpDataset);
 }
+
+var dataset = [
+    ['Country',  'Overall', 'Quality', 'Access', 'Administrative Efficiency', 'Equity', 'Health Care Outcomes', 'Health Spending per Capita'],
+    ['AUS', 1, 2, 4, 1, 7, 1, "$4,207"],
+    ['CAN', 9, 6, 10, 6, 9, 9, "$4,724"],
+    ['FRA', 10, 9, 9, 11,10, 5, "$4,620"],
+    ['GER', 8, 8, 2, 6, 6, 8, "$5,119"],
+    ['NETH', 3, 4, 1, 9, 2, 6, "$5,277"],
+    ['NZ', 4, 3, 7, 2, 8, 7, "$4,038"],
+    ['NOR', 5, 10, 5, 4, 5, 3, "$6,432"],
+    ['SWE', 6, 11, 6, 5, 3, 2, "$5,306"],
+    ['SWIZ', 6, 7, 8, 8, 4, 4, "$6,787"],
+    ['UK', 2, 1, 3, 3, 1, 10, "$4,094"],
+    ['US', 11, 5, 11, 10, 11, 11, "$9,364"],
+]
+
+
+
+console.log(dataset)
     
 d3.select("#testContainer")
     .append("table")
