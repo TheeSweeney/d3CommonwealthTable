0aa7d252a97d33dfe3cef1095b7af87214cba95a
Brian Sweeney <bsweeney930@gmail.com>
2017-05-01 23:25:36 -0400

    commented out logic for click to attempt sorting

--- app.js
+++ app.js
@@ -7,31 +7,30 @@ $( document ).ready(function(){
         
         $(element).find("tr:odd").addClass("odd");
         $(element).find("tr:not(.odd)").hide();
-        $(element).find("tr:first-child").show();
+        // $(element).find("tr:first-child").show();
 
-        $(element).find("#headerRow").addClass('odd')
+        // $(element).find("#headerRow").addClass('odd')
 
-        $(element).find("tr.odd").click(function() {
-            var alreadyActive = false;
-            if($(this).next("tr").find('td').attr('id') === 'activeRow') alreadyActive = true;
+        // $(element).find("tr.odd").click(function() {
+        //     var alreadyActive = false;
+        //     if($(this).next("tr").find('td').attr('id') === 'activeRow') alreadyActive = true;
 
-            $(element).find("td").attr('id', '');
-            $(this).next("tr").find('td').attr('id','activeRow')
-            $(element).find("tr:not(.odd)").hide();
-            $(this).next("tr").toggle();
+        //     $(element).find("td").attr('id', '');
+        //     $(this).next("tr").find('td').attr('id','activeRow')
+        //     $(element).find("tr:not(.odd)").hide();
+        //     $(this).next("tr").toggle();
 
-            if(alreadyActive){
-              $(this).next('tr').toggle();
-              $(this).next("tr").find('td').attr('id','')
-            }
+        //     if(alreadyActive){
+        //       $(this).next('tr').toggle();
+        //       $(this).next("tr").find('td').attr('id','')
+        //     }
 
-            createChart();
-        });
+        //     createChart();
+        // });
         
     }    
 })(jQuery); 
 
-// $('#exhibitTable').jExpand()
 $('#d3TableContainer').jExpand()
 $('#tableContainer').jExpand()
 
