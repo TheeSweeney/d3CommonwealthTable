12be4f636266872db87eb7a8d4ee1ba874430132
Brian Sweeney <bsweeney930@gmail.com>
2017-05-08 23:01:29 -0400

    cleaning up the code

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var margin = {
 };
 var width = w - margin.left - margin.right;
 var height = h - margin.top - margin.bottom;
+var activeSubsection;
 
 var createTable = function(params){
     d3.select("#d3TableContainer")
@@ -88,11 +89,6 @@ function createChart(dataSet){
             .range([height, 0])
   
 
-  function addQuestions(params){
-    this.selectAll('.select')
-        .data([params.questions])
-  }
-
   function plot(params){
 
     //enter
@@ -163,109 +159,127 @@ function createChart(dataSet){
 
 }
 
-function questionClick(data){
- console.log(data)
- console.log(subsectionData['QualityQuestions'][0].sectionData[0].data)
- createChart(data);
+var activeQuestion;
+
+function questionClick(d){
+
+  var questionId = d.q.split(' ').join('') + 'Id'
+  activeQuestion = questionId;
+
+  var activeSubsectionPassed = false;
+  d3.select("#" + activeSubsection)
+    .append('text')
+    .attr('id', function(){
+      return questionId;
+    })
+    .html(function(){
+      return d.q;
+    })
+  d3.selectAll('.subsectionBar')
+    .style('top', function(d,i){
+      var top = activeSubsectionPassed ? '50px' : '';
+      if(d3.select(this).attr('id') === activeSubsection) activeSubsectionPassed = true  
+      return top;
+    })
+
+  createChart(d.data);
 }
 
 function createQuestionSet(){
   $('.subsectionBar').hide();
 
-    var alreadyActive = false;
+  var alreadyActive = false;
 
-    if($(this).attr('id') !== activeSubsection){
-      activeSubsection = $(this).attr('id');
-      $('#' + $(this).attr('id')).show();
-      d3.select(this).style('height', '200px')
-      d3.select('#' + this.id.slice(0, -2) + 'QuestionSet').selectAll('.subsectionQuestions')
-        .data(function(d){
-          return d.sectionData
+  if($(this).attr('id') !== activeSubsection){
+    activeSubsection = $(this).attr('id');
+    $('#' + $(this).attr('id')).show();
+    d3.select(this).style('height', '200px')
+    d3.select('#' + this.id.slice(0, -2) + 'QuestionSet').selectAll('.subsectionQuestions')
+      .data(function(d){
+        return d.sectionData
+      })
+      .enter()
+        .append('div')
+        .text(function(d){
+          return d.q
         })
-        .enter()
-          .append('div')
-          .text(function(d){
-            return d.q
-          })
-          .style('height', '50px')
-          .classed('question', true)
-          .on('click', function(d){
-            questionClick.call(this, d.data)
-          })
+        .style('height', '50px')
+        .classed('question', true)
+        .on('click', function(d){
+          questionClick.call(this, d)
+        })
 
 
-      d3.selectAll('.questionSet')
-        .style('border-top', '3px solid rgb(255,96,0)')
-    }else{
-      d3.selectAll('.questionSet')
-        .html('')
-        .style('border-top', '')
-      d3.select(this).style('height', '20px')
-      activeSubsection = '';
-      $('.subsectionBar').show();
-    }
+    d3.selectAll('.questionSet')
+      .style('border-top', '3px solid rgb(255,96,0)')
+  }else{
+    d3.selectAll('.questionSet')
+      .html('')
+      .style('border-top', '')
+    d3.select(this).style('height', '20px')
+    activeSubsection = '';
+    $('.subsectionBar').show();
+  }
 }
 
-
-var activeSubsection;
 function createSubsections(rowId){
 
   var activeSubset = subsectionData[rowId.slice(0, -3) + 'Questions'];
 
   d3.select('.activeRow').selectAll('.subsectionBar')
-      .data(activeSubset)
-      .enter()
-        .append('text')
-        .html(function(d){
-          return d.questionSet;
+    .data(activeSubset)
+    .enter()
+      .append('text')
+      .html(function(d){
+        return d.questionSet;
+      })
+      .attr('id', function(d){
+        return (d.questionSet.split(' ').join('') + 'Id')
+      })
+      .style('height', '20px')
+      .style('width', '50%')
+      .style('position', 'relative')
+      .classed('subsectionBar', true)
+        .append('div')
+        .style('max-height', '90%')
+        .style('overflow', 'auto')
+        .classed('questionSet', true)
+        .attr('id', function(d, i){
+          return d.questionSet.split(' ').join('') + 'QuestionSet'
         })
-        .attr('id', function(d){
-          return (d.questionSet.split(' ').join('') + 'Id')
-        })
-        .style('height', '20px')
-        .style('width', '50%')
-        .style('position', 'relative')
-        .classed('subsectionBar', true)
-          .append('div')
-          .style('max-height', '90%')
-          .style('overflow', 'auto')
-          .classed('questionSet', true)
-          .attr('id', function(d, i){
-            return d.questionSet.split(' ').join('') + 'QuestionSet'
-          })
           
 
   d3.selectAll('.subsectionBar')
-  .on('click', function(){
-    createQuestionSet.call(this)
-  })
+    .on('click', function(){
+      createQuestionSet.call(this)
+    })
 }
 
 var activeRowId;
 function opentChart(data){
-      var alreadyActive = false;
-      if($(this).attr('id') === activeRowId) alreadyActive = true;
-      
-      var rowId = $(this).attr('id');
-      activeRowId = rowId
-        
-      d3.select('.activeRow').classed('activeRow', false);
+  var alreadyActive = false;
+  if($(this).attr('id') === activeRowId) alreadyActive = true;
+  
+  var rowId = $(this).attr('id');
+  activeRowId = rowId
+    
+  d3.select('.activeRow').classed('activeRow', false);
 
 
-      
-      $('#' + rowId).next("tr").find('td').addClass('activeRow')
-      $('#d3TableContainer').find("tr:not(.odd)").hide();
-      $('#' + rowId).next("tr").toggle();
-      
+  
+  $('#' + rowId).next("tr").find('td').addClass('activeRow')
+  $('#d3TableContainer').find("tr:not(.odd)").hide();
+  $('#' + rowId).next("tr").toggle();
+  
 
-      if(alreadyActive){
-        $('#' + rowId).next('tr').toggle();
-        $('#' + rowId).next("tr").find('td').attr('id','')
+  if(alreadyActive){
+    $('#' + rowId).next('tr').toggle();
+    $('#' + rowId).next("tr").find('td').attr('id','')
 
-        activeRowId = 'none'
-      }
+    activeRowId = 'none'
+  }
 
-      createSubsections.call(this, rowId);
+  createSubsections.call(this, rowId);
 }
 
 d3.selectAll('.odd')
